5b87fef8a81e1b67b79e9e30f377c89ce0c2095d
Sarvar Ergashev <ergashev.sarvar@uzcard.uz>
2024-04-13 13:19:11 +0500

    add: index

---
+++ components/EducationModal.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <Dialog v-model="isOpen" @close="closeModal">
+      <h2 slot="header">Add Education</h2>
+      <div slot="body">
+        <input v-model="school" placeholder="School" />
+        <input v-model="degree" placeholder="Degree" />
+        <input v-model="fieldOfStudy" placeholder="Field of Study" />
+        <input v-model="startDate" type="date" placeholder="Start Date" />
+        <input v-model="endDate" type="date" placeholder="End Date" />
+      </div>
+      <button slot="footer" @click="saveEducation">Save</button>
+    </Dialog>
+
+    <Dialog>
+      <DialogTrigger>
+        <button @click="openEditProfileDialog">Edit Profile</button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <button @click="saveProfileChanges">Save changes</button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  </div>
+</template>
+
+<script>
+// import { ref } from "@nuxtjs/composition-api";
+
+export default {
+  setup() {
+    const isOpen = ref(false);
+    const school = ref("");
+    const degree = ref("");
+    const fieldOfStudy = ref("");
+    const startDate = ref("");
+    const endDate = ref("");
+
+    const openEditProfileDialog = () => {
+      // Logic to open the edit profile dialog
+    };
+
+    const closeModal = () => {
+      isOpen.value = false;
+    };
+
+    const saveEducation = () => {
+      // Logic to save education data
+      closeModal();
+    };
+
+    const saveProfileChanges = () => {
+      // Logic to save profile changes
+    };
+
+    return {
+      isOpen,
+      school,
+      degree,
+      fieldOfStudy,
+      startDate,
+      endDate,
+      openEditProfileDialog,
+      closeModal,
+      saveEducation,
+      saveProfileChanges,
+    };
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,63 @@
 <template>
   <div>
-    <Button>Click me</Button>
+    <div class="container">
+      <h1>User Profile</h1>
+      <button @click="openEducationForm">Add Education</button>
+      <div v-if="educations.length > 0">
+        <div v-for="(education, index) in educations" :key="index">
+          <p>{{ education.school }}</p>
+          <p>{{ education.degree }}</p>
+          <p>{{ education.fieldOfStudy }}</p>
+          <p>{{ education.startDate }} - {{ education.endDate }}</p>
+          <button @click="removeEducation(index)">Remove</button>
+        </div>
+      </div>
+      <div v-else>
+        <p>No data</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  setup() {
-    return {};
+  data() {
+    return {
+      educations: [],
+    };
+  },
+  methods: {
+    async openEducationForm() {},
+    async fetchEducations() {
+      try {
+        const response = await fetch("@/static/educations.json");
+        const data = await response.json();
+        this.educations = data;
+      } catch (error) {
+        console.error("Error fetching educations:", error);
+      }
+    },
+    async removeEducation(index) {
+      this.educations.splice(index, 1);
+      await this.saveToJsonFile();
+    },
+    async saveToJsonFile() {
+      try {
+        await fetch("/api/save-educations", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this.educations),
+        });
+        console.log("Data saved to JSON file successfully.");
+      } catch (error) {
+        console.error("Error saving data to JSON file:", error);
+      }
+    },
+  },
+  async created() {
+    await this.fetchEducations();
   },
 };
 </script>
-
-<style lang="scss" scoped></style>
